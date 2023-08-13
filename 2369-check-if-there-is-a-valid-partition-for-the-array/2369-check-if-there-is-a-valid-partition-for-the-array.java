07ac15430e81289b028ed4a78c2d8250e009692c
Akhil Dubey <94775233+dubeyx@users.noreply.github.com>
2023-08-13 13:39:19 +0530

    Time: 6 ms (92.23%), Space: 59.9 MB (42.72%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -1,15 +1,46 @@
 class Solution {
-    public boolean validPartition(int[] A) {
-        int n = A.length, dp[] = new int[] {0, 0, 0, 1};
-        for (int i = 0; i < n; ++i) {
-            dp[i % 4] = 0;
-            if (i - 1 >= 0 && A[i] == A[i - 1])
-                dp[i % 4] |= dp[(i + 2) % 4];
-            if (i - 2 >= 0 && A[i] == A[i - 1] && A[i - 1] == A[i - 2])
-                dp[i % 4] |= dp[(i + 1) % 4];
-            if (i - 2 >= 0 && A[i] - 1 == A[i - 1] && A[i - 1] == A[i - 2] + 1)
-                dp[i % 4] |= dp[(i + 1) % 4];
-        }
-        return dp[(n - 1) % 4] > 0;
+    int[] dp;
+    public boolean validPartition(int[] nums) {
+        dp= new int[nums.length+1];
+        
+        
+        return helper(nums, 0);
     }
+    
+    public boolean helper(int[] nums, int i){
+        
+        if(i>=nums.length) return true;
+        
+        if(dp[i]>0) return dp[i]==1?true:false;
+        
+        if(i+1<nums.length && nums[i]==nums[i+1]){
+            boolean a=false;
+            boolean b=false;
+            
+            if(i+2<nums.length && nums[i+1]==nums[i+2]) a= helper(nums,i+3);
+            
+            b=helper(nums,i+2);
+            
+            if(b==true) dp[i]=1;
+            else dp[i]=2;
+            
+            return a||b;
+            
+        }
+        
+        
+        if(i+2<nums.length && nums[i]==nums[i+1]-1 && nums[i+1]==nums[i+2]-1){
+            
+            boolean x= helper(nums,i+3);
+                dp[i]=(x==true)?1:2;
+            return x;
+        }
+        
+        dp[i]=2;
+        return false;
+        
+    }
+    
+    
+    
 }
